8c9fe4207f5c18226e16c24e159c412b8be70ea5
hammonds <hammonds@lambda.xray.aps.anl.gov>
2016-02-15 11:11:30 -0600

    Change value of some parameters.  Changes up some missed things from copy of PiCam files.  Add in PipeWriter plugin

--- iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
+++ iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
@@ -2,15 +2,15 @@ errlogInit(20000)
 
 < envPaths
 #epicsThreadSleep(20)
-dbLoadDatabase("$(TOP)/dbd/PICamApp.dbd")
-PICamApp_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase("$(TOP)/dbd/LambdaApp.dbd")
+LambdaApp_registerRecordDeviceDriver(pdbbase) 
 
 # Prefix for all records
-epicsEnvSet("PREFIX", "13LAMBDA1:")
+epicsEnvSet("PREFIX", "8LAMBDA1:")
 # The port name for the detector
 epicsEnvSet("PORT",   "LAMBDA1")
 # The queue size for all plugins
-epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("QSIZE",  "30")
 # The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "2048")
 # The maximim image height; used for column profiles in the NDPluginStats plugin
@@ -21,18 +21,18 @@ epicsEnvSet("NCHANS", "2048")
 epicsEnvSet("CBUFFS", "500")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
-
+#epicsThreadSleep(15)
 # Create a PICam driver
-# PICamConfig(const char *portName, IDType, IDValue, maxBuffers, size_t maxMemory, int priority, int stackSize)
+# LambdaConfig(const char *portName, const char * configPath, IDType, IDValue, maxBuffers, size_t maxMemory, int priority, int stackSize)
 
 # This is for a 
-LambdaConfig("$(PORT)", 0, 0, 0, 0)
+LambdaConfig("$(PORT)", "config",  0, 0, 0, 0)
 
 asynSetTraceIOMask($(PORT), 0, 2)
 #asynSetTraceMask($(PORT),0,0xff)
 
 dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADPICAM)/db/ADLambda.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADLAMBDA)/db/ADLambda.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from Driver.
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
@@ -41,7 +41,12 @@ dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=I
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
-set_requestfile_path("$(ADPICAM)/PICamApp/Db")
+
+#Note mpi control pipe out & in reversed.  Names are from the view of the MPI program.
+NDPipeWriterConfigure("PipeWriter1", 4000, 0, "$(PORT)", "/local/xpcscmdout", "/local/xpcscmdin", 0, 0, 0, 0,0)
+dbLoadRecords("$(ADCORE)/db/NDPluginPipeWriter.template", "P=$(PREFIX),R=PW1:,  PORT=PipeWriter1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),CMD_IN_PORT=PW_CMD_IN,CMD_OUT_PORT=PW_CMD_OUT")
+set_requestfile_path("$(ADLAMBDA)/LambdaApp/Db")
+set_requestfile_path("$(NDPLUGINPIPEWRITER)/PipeWriterApp/Db")
 
 #asynSetTraceMask($(PORT),0,0x09)
 #asynSetTraceMask($(PORT),0,0x11)
