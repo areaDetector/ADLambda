8f549f20673a6aba431a53a4add21f452d491007
hammonds <hammonds@lambda.xray.aps.anl.gov>
2016-02-29 10:29:57 -0600

    Changes to add in PipeWriter plugin.

--- iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
+++ iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
@@ -2,8 +2,9 @@
 cd /local/epics/synApps_5_8/support/areaDetector-R2-0/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
 setenv EPICS_DISPLAY_PATH ../../../../LambdaApp/op/adl:../../../../../ADCore-R2-2/ADApp/op/adl:../../../../../NDPluginPipeWriter/PipeWriterApp/op/adl:../../../../../../asyn-4-26/opi/medm
 
-medm -attach -x -macro "P=8LAMBDA1:,R=cam1:" LambdaBase.adl &
+medm -x -macro "P=8LAMBDA1:,R=cam1:" LambdaBase.adl &
 sleep 1
-medm -attach -x -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
+medm -x -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
+#medm -attach -x -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
 #medm -attach -x -macro "P=8LAMBDA1:,R=PW1:" MPI.adl &
 
