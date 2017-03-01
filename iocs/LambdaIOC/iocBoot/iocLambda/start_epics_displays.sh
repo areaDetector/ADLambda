683f9f94429d759fdec2ce2fa863cce4c541d240
John Hammonds <JPHammonds@anl.gov>
2017-03-01 12:53:57 -0600

    Changes Made going from AD 2.0 to 2.5

--- iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
+++ iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
-cd /local/epics/synApps_5_8/support/areaDetector-R2-0/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
-setenv EPICS_DISPLAY_PATH ../../../../LambdaApp/op/adl:../../../../../ADCore-R2-2/ADApp/op/adl:../../../../../NDPluginPipeWriter/PipeWriterApp/op/adl:../../../../../../asyn-4-26/opi/medm
+cd /local/epics/synApps_5_8/support/areaDetector-R2-5/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
+setenv EPICS_DISPLAY_PATH ../../../../LambdaApp/op/adl:../../../../../ADCore-R2-5/ADApp/op/adl:../../../../../NDPluginPipeWriter/PipeWriterApp/op/adl:../../../../../../asyn-4-30/opi/medm:../../../../../NDPluginFileIMM/NDFileIMMApp/op/adl
 
 medm -x -macro "P=8LAMBDA1:,R=cam1:" LambdaBase.adl &
 sleep 1
