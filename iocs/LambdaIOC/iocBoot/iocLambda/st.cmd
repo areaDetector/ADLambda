5a44d987269b9a5b981d098dc848d61196108418
John Hammonds <JPHammonds@anl.gov>
2017-03-01 12:57:02 -0600

    Go to AD 2.5 and add the IMM file plugin.

--- iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
+++ iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
@@ -27,27 +27,33 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # This is for a 
 LambdaConfig("$(PORT)", "config",  0, 0, 0, 0)
+epicsThreadSleep(2)
 
 asynSetTraceIOMask($(PORT), 0, 2)
 #asynSetTraceMask($(PORT),0,0xff)
 
 dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ADLAMBDA)/db/ADLambda.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-
+#
 # Create a standard arrays plugin, set it to get data from Driver.
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
 dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,SIZE=16,FTVL=SHORT,NELEMENTS=20000000")
-
+#
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
-
+#
 #Note mpi control pipe out & in reversed.  Names are from the view of the MPI program.
-NDPipeWriterConfigure("PipeWriter1", 4000, 0, "$(PORT)", "/local/xpcscmdout", "/local/xpcscmdin", 0, 0, 0, 0,0)
+NDPipeWriterConfigure("PipeWriter1", 15000, 0, "$(PORT)", "/local/xpcscmdout", "/local/xpcscmdin", 0, 0, 0, 0,0)
 dbLoadRecords("$(ADCORE)/db/NDPluginPipeWriter.template", "P=$(PREFIX),R=PW1:,  PORT=PipeWriter1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),CMD_IN_PORT=PW_CMD_IN,CMD_OUT_PORT=PW_CMD_OUT")
+
+#Note Local plugin to run the IMM plugin writer
+NDFileIMMConfigure("IMM1", 15000, 0, "$(PORT)",  0, 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDFileIMM.template", "P=$(PREFIX),R=IMM1:,PORT=IMM1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
 
 set_requestfile_path("$(ADLAMBDA)/LambdaApp/Db")
 set_requestfile_path("$(NDPLUGINPIPEWRITER)/PipeWriterApp/Db")
+set_requestfile_path("$(NDPLUGINFILEIMM)/NDFileIMMApp/Db")
 
 #asynSetTraceMask($(PORT),0,0x09)
 #asynSetTraceMask($(PORT),0,0x11)
