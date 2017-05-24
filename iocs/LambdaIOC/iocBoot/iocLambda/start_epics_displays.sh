ceae031df5636c47bc2b2b9d734f44e2ca93d469
John Hammonds <JPHammonds@anl.gov>
2017-05-24 13:53:25 -0500

    Comment out pipewriter plugin and add in separated Collect plugin. Change directories to reflect version changes.

--- iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
+++ iocs/LambdaIOC/iocBoot/iocLambda/start_epics_displays.sh
@@ -1,10 +1,11 @@
 #!/bin/tcsh
-cd /local/epics/synApps_5_8/support/areaDetector-R2-5/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
-setenv EPICS_DISPLAY_PATH ../../../../LambdaApp/op/adl:../../../../../ADCore-R2-5/ADApp/op/adl:../../../../../NDPluginPipeWriter/PipeWriterApp/op/adl:../../../../../../asyn-4-30/opi/medm:../../../../../NDPluginFileIMM/NDFileIMMApp/op/adl
+cd /local/epics/synApps_5_8/support/areaDetector-R3-0/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
+setenv EPICS_DISPLAY_PATH ../../../../LambdaApp/op/adl:../../../../../ADCore-R3-0/ADApp/op/adl:../../../../../NDPluginPipeWriter/PipeWriterApp/op/adl:../../../../../../asyn-4-30/opi/medm:../../../../../NDPluginFileIMM/NDFileIMMApp/op/adl
 
 medm -x -macro "P=8LAMBDA1:,R=cam1:" LambdaBase.adl &
 sleep 1
-medm -x -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
-#medm -attach -x -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
-#medm -attach -x -macro "P=8LAMBDA1:,R=PW1:" MPI.adl &
+#medm -x -attach -macro "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
+medm -x -macro "P=8LAMBDA1:,R=cam1:" LambdaCollect.adl &
+#medm -attach -x  "P=8LAMBDA1:,R=PW1:" NDPluginPipeWriter.adl &
+#medm -attach -x  "P=8LAMBDA1:,R=PW1:" MPI.adl &
 
