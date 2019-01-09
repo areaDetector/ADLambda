751cd59224126f1e156a54c11c0b9b0a27ef8345
John Hammonds <JPHammonds@anl.gov>
2019-01-09 11:35:53 -0600

    Get rid of pipewriter stuff.  Update version numbers in search path. Add LD_LIBRARY_PATH to find custom boost library and the detector libraries.

--- iocs/LambdaIOC/iocBoot/iocLambda/startIOC.sh
+++ iocs/LambdaIOC/iocBoot/iocLambda/startIOC.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 setenv EPICS_CA_MAX_ARRAY_BYTES 10000000
-
-cd /local/epics/synApps_5_8/support/areaDetector-R3-0/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
+setenv LD_LIBRARY_PATH /local/boost-build/lib/:/local/epics/synApps_6_0/support/areaDetector-R3-4/ADLambda/lib/linux-x86_64
+cd /local/epics/synApps_6_0/support/areaDetector-R3-4/ADLambda/iocs/LambdaIOC/iocBoot/iocLambda
 
 ../../bin/linux-x86_64/LambdaApp st.cmd
