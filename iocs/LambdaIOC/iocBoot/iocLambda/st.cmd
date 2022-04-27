6ad3ff6938deec273827334201a08cb94f438cd7
Keenan Lang <klang@aps.anl.gov>
2022-04-27 11:38:48 -0500

    Use iocsh file

--- iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
+++ iocs/LambdaIOC/iocBoot/iocLambda/st.cmd
@@ -5,43 +5,8 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/LambdaApp.dbd")
 LambdaApp_registerRecordDeviceDriver(pdbbase) 
 
-# Prefix for all records
-epicsEnvSet("PREFIX", "8LAMBDA1:")
-# The port name for the detector
-epicsEnvSet("PORT",   "LAMBDA1")
-# The queue size for all plugins
-epicsEnvSet("QSIZE",  "30")
-# The maximim image width; used for row profiles in the NDPluginStats plugin
-epicsEnvSet("XSIZE",  "2048")
-# The maximim image height; used for column profiles in the NDPluginStats plugin
-epicsEnvSet("YSIZE",  "2048")
-# The maximum number of time seried points in the NDPluginStats plugin
-epicsEnvSet("NCHANS", "2048")
-# The maximum number of frames buffered in the NDPluginCircularBuff plugin
-epicsEnvSet("CBUFFS", "1000")
-# The search path for database files
-epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
-#epicsThreadSleep(15)
 
-LambdaConfig("$(PORT)", "config",  0, 0, 0, 0)
-epicsThreadSleep(2)
-
-asynSetTraceIOMask($(PORT), 0, 2)
-#asynSetTraceMask($(PORT),0,0xff)
-
-dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADLAMBDA)/db/ADLambda.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-
-dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam2:,PORT=$(PORT),ADDR=1,TIMEOUT=1")
-dbLoadRecords("$(ADLAMBDA)/db/ADLambda.template","P=$(PREFIX),R=cam2:,PORT=$(PORT),ADDR=1,TIMEOUT=1")
-#
-# Create a standard arrays plugin, set it to get data from Driver.
-NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=802896")
-#
-# Load all other plugins using commonPlugins.cmd
-< $(ADCORE)/iocBoot/commonPlugins.cmd
+iocshLoad("ADLambda.iocsh", "PREFIX=LAMBDA1:, PORT=LAMBDA1")
 
 set_requestfile_path("$(ADLAMBDA)/LambdaApp/Db")
 
